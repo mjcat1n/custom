69ebcf80cab0e985f1de53a2bc3cc7e2c20fed14
Matt <mjcantin@yahoo.com>
2016-07-15 03:45:53 -0600

    Update extension.js

--- extension.js
+++ extension.js
@@ -59,7 +59,7 @@
       smartSkip: true,
       cmdDeletion: true,
       maximumAfk: 120,
-      afkRemoval: true,
+      afkRemoval: false,
       maximumDc: 60,
       bouncerPlus: true,
       blacklistEnabled: true,
